0765a1ae23bae9a0e2c125efd53c9dc74d3859c9
Agastya <valisettyagastya00@gmail.com>
2026-08-06 23:18:21 +0530

    Merge remote-tracking branch 'origin/main' -started modularising code -created Orderbook.h, Orderbook.cpp -created Order and Usings header files -clearning code and splitting it

---
+++ Order.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <list>
+#include <exception>
+#include <format>
+
+#include "OrderType.h"
+#include "Side.h"
+#include "Usings.h"
+#include "Constants.h"
---
+++ Orderbook.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Agastya on 06-08-2026.
+//
+
+#include "Orderbook.h"
