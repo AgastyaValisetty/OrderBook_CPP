98842c9b4922bc55adc695ae1d437c926317709b
Agastya <valisettyagastya00@gmail.com>
2026-08-06 23:38:12 +0530

    modified constants order ordertype side and usings header files, unresolved errors in order.h remaining

---
+++ Constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <limits>
+
+#include "Usings.h"
+
+struct Constants {
+
+    static const Price InvalidPrice = -1;
+};
--- Order.h
+++ Order.h
@@ -8,3 +8,30 @@
 #include "Side.h"
 #include "Usings.h"
 #include "Constants.h"
+
+
+class Order {
+public:
+    Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
+        : orderType_ (orderType)
+        , orderId_ (orderId)
+        , side_ (side)
+        , price_ (price)
+        , initialQuantity_ (quantity)
+        , remainingQuantity_ (quantity)
+    {}
+
+    Order(OrderId orderId, Side side, Quantity quantity)
+        : Order(OrderType::Market, orderId, side, Constants::InvalidPrice, quantity)
+    {}
+
+    OrderId GetOrderId() const { return orderId_; }
+    Side GetSide() const { return side_; }
+    Price GetPrice() const { return price_; }
+    OrderType GetOrderType() const { return orderType_; }
+    Quantity GetInitialQuantity() const { return initialQuantity_; }
+    Quantity GetRemainingQuantity() const { return remainingQuantity_; }
+    bool isFilled() const { return GetInitialQuantity - remainingQuantity_; }
+
+
+};
