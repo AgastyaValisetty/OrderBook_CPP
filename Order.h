1883843fe35d36c606a6898cc8a3a0fa3f343057
Agastya <valisettyagastya00@gmail.com>
2026-08-07 21:09:56 +0530

    Order.h completed

--- Order.h
+++ Order.h
@@ -10,20 +10,22 @@
 #include "Constants.h"
 
 
+
 class Order {
 public:
     Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
-        : orderType_ (orderType)
-        , orderId_ (orderId)
-        , side_ (side)
-        , price_ (price)
-        , initialQuantity_ (quantity)
-        , remainingQuantity_ (quantity)
-    {}
+        : orderType_{ orderType }
+        , orderId_{ orderId }
+        , side_{ side }
+        , price_{ price }
+        , initialQuantity_{ quantity }
+        , remainingQuantity_{ quantity }
+    { }
 
     Order(OrderId orderId, Side side, Quantity quantity)
         : Order(OrderType::Market, orderId, side, Constants::InvalidPrice, quantity)
     {}
+
 
     OrderId GetOrderId() const { return orderId_; }
     Side GetSide() const { return side_; }
@@ -31,7 +33,27 @@ public:
     OrderType GetOrderType() const { return orderType_; }
     Quantity GetInitialQuantity() const { return initialQuantity_; }
     Quantity GetRemainingQuantity() const { return remainingQuantity_; }
-    bool isFilled() const { return GetInitialQuantity - remainingQuantity_; }
+    Quantity GetFilledQuantity() const{ return (GetInitialQuantity() - GetRemainingQuantity()); }
+    bool IsFilled() const { return GetRemainingQuantity() == 0; }
+    void Fill(Quantity quantity) {
+        if ( quantity > GetRemainingQuantity() ) throw std::logic_error(std::format("Order  ({}) cannot be filled for more than its remaining quantity.", GetOrderId()));
+        remainingQuantity_ -= quantity;
+    }
+    void ToGoodTillCancel(Price price) {
+        if (GetOrderType() != OrderType::Market) throw std::logic_error(std::format("Order type ({}) is not a Market.", GetOrderId()));
 
+        price_ = price;
+        orderType_ = OrderType::GoodTillCancel;
+    }
 
+private:
+    OrderType orderType_;
+    OrderId orderId_;
+    Side side_;
+    Price price_;
+    Quantity initialQuantity_;
+    Quantity remainingQuantity_;
 };
+
+using OrderPointer = std::shared_ptr<Order>;
+using OrderPointers = std::shared_ptr<OrderPointer>;
